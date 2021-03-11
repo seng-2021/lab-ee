259a8127201b1069ac76437f62177255851192bc
eemelihayrynen <55876843+eemelihayrynen@users.noreply.github.com>
2021-03-11 23:56:32 +0200

    Update mycrypt.py

--- lab2/mycrypt.py
+++ lab2/mycrypt.py
@@ -24,7 +24,8 @@ def encode(s):
             crypted+=codecs.encode(c,'rot13')
         elif c in digitmapping:
           crypted+=digitmapping[c]
-        
+        else:
+            TypeError
     return crypted[:origlen]
 
 def decode(s):
