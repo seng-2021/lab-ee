c9e3be801bbf457526471cbf84e3aff958db7c5c
eemelihayrynen <55876843+eemelihayrynen@users.noreply.github.com>
2021-03-11 22:48:18 +0200

    Update mycrypt.py

--- lab2/mycrypt.py
+++ lab2/mycrypt.py
@@ -1,26 +1,23 @@
 import codecs
-
 def encode(s):
-    try:
-        isinstance(s,str):
-    except TypeError:
-        raise
+    if not in isinstance(s,str):        
+        raise TypeError
     origlen = len(s)
     crypted = ""
     digitmapping = dict(zip('1234567890!"#€%&/()=','!"#€%&/()=1234567890'))
+    s = s.ljust(1000,"v")
     if len(s) > 1000:
         raise ValueError
     for c in s:
         if c.isalpha():
-            if c.islower():
+            if c.isupper():
+                c=c.lower()
+            elif c.islower():
                 c=c.upper()
             # Rot13 the character for maximum security
             crypted+=codecs.encode(c,'rot13')
         elif c in digitmapping:
           crypted+=digitmapping[c]
-
-    return crypted
-
+    return crypted[:origlen]
 def decode(s):
-    return s
-
+    return encode(s)
