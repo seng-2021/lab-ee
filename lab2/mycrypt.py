3738f3473a0a437e1921d3d8e0ac2d4ed26abad2
eemelihayrynen <55876843+eemelihayrynen@users.noreply.github.com>
2021-03-12 01:36:47 +0200

    Update mycrypt.py

--- lab2/mycrypt.py
+++ lab2/mycrypt.py
@@ -9,16 +9,16 @@ def encode(s):
    
     digitmapping = dict(zip('1234567890!"#€%&/()=','!"#€%&/()=1234567890'))
     if len(s) > 1000:
-        raise ValueError    
+        raise ValueError
     s = s.ljust(1000, "c")
     for c in s:
-        if c in ["ä","ö","å","+"]:
+        if c in ["å","ä","ö"]:
             raise ValueError
         elif c.isalpha():
             if c.islower():
                 c=c.upper()
             elif c.isupper():
-                c=c.islower()
+                c=c.lower()
             # Rot13 the character for maximum security
             crypted+=codecs.encode(c,'rot13')
         elif c in digitmapping:
